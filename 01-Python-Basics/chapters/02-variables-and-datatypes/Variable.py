f8754248bdcaa904559b47806403a5c709abe415
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2025-12-05 14:11:28 +0530

    Update Variable.py

--- 01-Python-Basics/chapters/02-variables-and-datatypes/Variable.py
+++ 01-Python-Basics/chapters/02-variables-and-datatypes/Variable.py
@@ -1,16 +1,18 @@
-a = 5   # 5 is identifior
+# 'a' and 'b' are identifiers (variable names)
+# 5 and 3 are literal values stored inside variables
 
+a = 5
 b = 3
 
-print(a+b)
-print(a*b)
+print(a + b)
+print(a * b)
 
-
-
-# varibles like a = continar and  5 = identifiar
-
+# Variables act like containers that hold data
+# Here 'name' is the identifier and "umesh" is the value stored inside it
 
 name = "umesh"
-print(name,type(name))
-print(a,type(a))
+
+print(name, type(name))
+print(a, type(a))
+
 
