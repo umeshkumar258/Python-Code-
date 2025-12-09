47b252ca346c04548ec3f3f13f4fe6fa0b255945
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2025-12-09 18:23:02 +0530

    Rename variable and add comments in sets.py
    Updated variable name from 'set' to 'my_set' to avoid naming conflict. Added comments explaining set properties and other data types.

--- 01-Python-Basics/chapters/05-dictionary-and-sets/03 sets.py
+++ 01-Python-Basics/chapters/05-dictionary-and-sets/03 sets.py
@@ -1,27 +1,25 @@
-set = {37,393,283,29,66}
-
-print(type(set),set)
-
-
-
-a = {8}
-print(type(a))
-
-b = ()
-print(type(b))
-
-c = {}
-print(type(c))
-
-d = []
-print(type(d))
-
-e = ""
-print(type(e))
-
-# Sets are unordered => Element’s order doesn’t matter
-# 2. Sets are unindexed => Cannot access elements by index
-# 3. There is no way to change items in sets.
-# 4. Sets cannot contain duplicate values.
+# Defining a set (avoid naming variable as set)
+my_set = {37, 393, 283, 29, 66}
+print("my_set:", my_set)
+print("Type of my_set:", type(my_set))
 
 
+# Other data types
+a = {8}          # This is a set
+b = ()           # This is a tuple
+c = {}           # This is a dictionary, NOT a set
+d = []           # This is a list
+e = ""           # This is a string
+
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+print("Type of c:", type(c))
+print("Type of d:", type(d))
+print("Type of e:", type(e))
+
+
+# Set Properties:
+# 1. Sets are unordered => element order doesn’t matter
+# 2. Sets are unindexed => cannot access elements using index
+# 3. Sets are immutable (you can add/remove elements, but existing elements cannot change)
+# 4. Sets cannot contain duplicate values
