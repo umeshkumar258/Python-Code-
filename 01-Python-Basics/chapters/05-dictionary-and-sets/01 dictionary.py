0cda36ec40287c4b0a839b87f24c1aea6ef74727
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2025-12-09 18:21:50 +0530

    Refactor dictionary example with comments and access methods
    Updated dictionary example to include comments and demonstrate key properties.

--- 01-Python-Basics/chapters/05-dictionary-and-sets/01 dictionary.py
+++ 01-Python-Basics/chapters/05-dictionary-and-sets/01 dictionary.py
@@ -1,22 +1,33 @@
-a = { "umesh": "73", "bab":"99","harry":"72"}
+# Dictionary creation
+marks = {
+    "umesh": "73",
+    "bab": "99",
+    "harry": "72"
+}
 
-print(type(a),a)
+# Display dictionary and its type
+print("Dictionary:", marks)
+print("Type:", type(marks))
 
+# Key properties of dictionaries:
+# 1. Unordered (until Python 3.7: now maintains insertion order but conceptually unordered)
+# 2. Mutable
+# 3. Indexed by keys
+# 4. Duplicate keys are not allowed
 
-# 1. It is unordered.
-# 2. It is mutable.
-# 3. It is indexed.
-# 4. Cannot contain duplicate keys.
+# Accessing values using keys
+print("Umesh's marks:", marks["umesh"])
+print("Length of dictionary:", len(marks))
 
+# Accessing value using get() method (safer method)
+print("Bab's marks:", marks["bab"])
+print("Bab's marks (using get):", marks.get("bab"))
 
-print(a["umesh"])   #not allowed a[0] as it is unordered
+# Dictionary with list value
+student_data = {
+    "numbers": [939, 9303, 99],
+    "umesh": "939"
+}
 
-print(len(a))
-
-print(a["bab"])
-print(a.get("bab"))     # both are same
-
-list1 = {"numbers": [939, 9303, 99], "umesh": "939"}
-
-print(list1)
-print(list1["numbers"])
+print("Student Data:", student_data)
+print("Numbers list:", student_data["numbers"])
