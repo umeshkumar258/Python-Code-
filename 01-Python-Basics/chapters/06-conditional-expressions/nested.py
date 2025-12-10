cdada08eeae18115c7c7e4ffc09e893d2d961918
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2025-12-10 16:38:01 +0530

    Fix indentation and improve print statements

--- 01-Python-Basics/chapters/06-conditional-expressions/nested.py
+++ 01-Python-Basics/chapters/06-conditional-expressions/nested.py
@@ -1,11 +1,9 @@
-age = int(input("Enter the age:"))
+age = int(input("Enter the age: "))
 
 if age >= 18:
     if age == 20:
         print("You can enter the event.")
-
     else:
-       print("you need a ticket to enter:")
-
+        print("You need a ticket to enter.")
 else:
-    print("you are too young to attend the event")
+    print("You are too young to attend the event.")
