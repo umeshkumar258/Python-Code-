ff153cf72a507086f802d49b63fb30e295cd5459
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2025-12-11 11:48:13 +0530

    Refactor while loop examples with comments
    Updated while loop examples to include comments and correct conditions.

--- 01-Python-Basics/chapters/07-loops/02 while loops.py
+++ 01-Python-Basics/chapters/07-loops/02 while loops.py
@@ -1,16 +1,19 @@
-i = 1  # i = 1 is the starting point
+# Print numbers from 1 to 50 using a while loop
+i = 1  # Starting point
 
-while(i<51):
+while i < 51:
     print(i)
-    i += 1 # i = i + 1 # This is  very important other wise its go infinty
+    i += 1   # Increase i by 1 (very important to avoid an infinite loop)
 
 
-i = 2
-while(i<15 and i < 10):
-    print(i)
-    i +=2  # this is incremets of by 2
+# Print even numbers using a while loop
+j = 2  # Start from 2
+
+# This condition means: run only when both conditions are true
+while j < 15 and j < 10:
+    print(j)
+    j += 2   # Increase by 2 each time
 
 
-
-
-# The logical opertaor      and or not can be used in loops and conditonal experssion
+# Note:
+# Logical operators (and, or, not) can be used inside loops and conditions.
