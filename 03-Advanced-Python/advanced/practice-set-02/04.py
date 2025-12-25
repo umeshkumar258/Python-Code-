efda530bfac859fb89803b962c0313e8bddc3d1a
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2025-12-25 17:49:52 +0530

    Refactor divisible function and add filtering methods
    Refactored function to check divisibility by 5 and added multiple methods to filter the list.

--- 03-Advanced-Python/advanced/practice-set-02/04.py
+++ 03-Advanced-Python/advanced/practice-set-02/04.py
@@ -1,11 +1,29 @@
-def divisible5(n):
-    if(n%3 == 0):
-        return True
-    return False
+# ---------------------------------------
+# FUNCTION TO CHECK DIVISIBLE BY 5
+# ---------------------------------------
+def divisible_by_5(n):
+    return n % 5 == 0
 
-a = [1,2,333,45,77,7,6665,6663,55,65,75]
+
+# Given list
+a = [1, 2, 333, 45, 77, 7, 6665, 6663, 55, 65, 75]
+
+# ---------------------------------------
+# 1️⃣ Using filter() with function
+# ---------------------------------------
+result_filter = list(filter(divisible_by_5, a))
+print("Divisible by 5 (filter + function):", result_filter)
 
 
-f = list(filter(divisible5,a))
-print(f)
+# ---------------------------------------
+# 2️⃣ Using lambda with filter()
+# ---------------------------------------
+result_lambda = list(filter(lambda x: x % 5 == 0, a))
+print("Divisible by 5 (filter + lambda):", result_lambda)
 
+
+# ---------------------------------------
+# 3️⃣ Using list comprehension (MOST PYTHONIC)
+# ---------------------------------------
+result_comp = [x for x in a if x % 5 == 0]
+print("Divisible by 5 (list comprehension):", result_comp)
