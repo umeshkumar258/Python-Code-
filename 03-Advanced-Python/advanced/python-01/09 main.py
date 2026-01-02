9231cc7af40a3ee40a41b8d6b57fad83195493e0
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-01-02 18:59:56 +0530

    Refactor main.py to include function import and call

--- 03-Advanced-Python/advanced/python-01/09 main.py
+++ 03-Advanced-Python/advanced/python-01/09 main.py
@@ -1,3 +1,11 @@
+# main.py
+# Importing a specific function from module.py
+
 from module import myfunc
 
+def main():
+    # Calling the imported function
+    myfunc()
 
+# Program starts here
+main()
