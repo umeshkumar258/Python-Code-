035ddb63d42baa71dc4e84fbaa73718f060756af
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-01-02 18:58:44 +0530

    Refactor try-except-finally in main function
    Refactor try-except-finally structure to improve clarity and add comments.

--- 03-Advanced-Python/advanced/python-01/08 try finally.py
+++ 03-Advanced-Python/advanced/python-01/08 try finally.py
@@ -1,27 +1,25 @@
-# try:
-#     a = int(input("Hey,enter a number:"))
-#     print(a)  
-
-
-# except Exception as e:
-#     print(e)
-
-# finally:
-#     print("hey i am inside finnlh")
-
 def main():
     try:
-        a = int(input("eneter the number: "))
-        print(a)
+        # Taking input from the user
+        a = int(input("Enter the number: "))
+        
+        # Printing the number if input is valid
+        print("You entered:", a)
+        
+        # Return statement (function tries to exit)
         return
-    
+
     except Exception as e:
-        print(e)
+        # This block executes if an error occurs
+        print("Error:", e)
+        
+        # Return statement (function tries to exit)
         return
-    
 
     finally:
-        print("hey i a inside")
+        # This block ALWAYS executes
+        # Even if return is present in try or except
+        print("Finally block executed")
 
-
+# Function call
 main()
