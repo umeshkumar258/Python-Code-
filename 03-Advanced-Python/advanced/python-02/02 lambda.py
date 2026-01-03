8df946868d07cf843ea8660ca3969372b5d63013
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-01-03 11:21:24 +0530

    Implement square calculation using function and lambda
    Added a normal function and a lambda function to calculate the square of a number, along with test prints for both methods.

--- 03-Advanced-Python/advanced/python-02/02 lambda.py
+++ 03-Advanced-Python/advanced/python-02/02 lambda.py
@@ -1,8 +1,14 @@
-# def square(n):
-#     return n*n
+# Normal function to find square
+def square_function(n):
+    return n * n
 
-# print(square(5))
 
-square = lambda n: n*n
+# Lambda function to find square
+square_lambda = lambda n: n * n
 
-print(square(7))
+
+# Testing both methods
+num = 7
+
+print("Square using normal function :", square_function(num))
+print("Square using lambda function :", square_lambda(num))
