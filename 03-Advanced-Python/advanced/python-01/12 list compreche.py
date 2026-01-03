5dd9f82c7695a7da160fbe84539b84433dc355bc
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-01-03 11:19:39 +0530

    Refactor list squaring examples with comments
    Refactor list squaring methods and improve output formatting.

--- 03-Advanced-Python/advanced/python-01/12 list compreche.py
+++ 03-Advanced-Python/advanced/python-01/12 list compreche.py
@@ -1,11 +1,19 @@
-mylist = [8,4,3,7,9]
+# Original list
+my_list = [8, 4, 3, 7, 9]
 
-# squardList = []
-# for item in mylist:
-#     squardList.append(item*item)
+# 1️⃣ Using normal for loop
+squared_list_loop = []
+for num in my_list:
+    squared_list_loop.append(num ** 2)
 
+# 2️⃣ Using list comprehension (Pythonic way)
+squared_list_comprehension = [num ** 2 for num in my_list]
 
-# This is another way getting the answer
+# 3️⃣ Using map() and lambda
+squared_list_map = list(map(lambda x: x ** 2, my_list))
 
-squardList = [i*i for i in mylist]
-print(squardList)
+# Output
+print("Original list              :", my_list)
+print("Squared list (for loop)    :", squared_list_loop)
+print("Squared list (comprehension):", squared_list_comprehension)
+print("Squared list (map method)  :", squared_list_map)
