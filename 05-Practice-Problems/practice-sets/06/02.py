1f822743aaeb807af48a7dfdc11017c41b9dd741
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-01-17 10:16:13 +0530

    Fix input variables and improve average calculation
    Updated variable names and adjusted average calculation logic.

--- 05-Practice-Problems/practice-sets/06/02.py
+++ 05-Practice-Problems/practice-sets/06/02.py
@@ -1,10 +1,15 @@
-a, d, c = [int(input("Enter your number: ")) for _ in range(3)]
+# Taking 3 numbers using list comprehension
+a, b, c = [int(input("Enter your number: ")) for _ in range(3)]
 
-average = (a + d + c) / 3
+# Calculating average
+average = (a + b + c) / 3
 
+# Result
 if average > 40:
     print("He scores good")
-elif average > 33:
+elif average >= 33:
     print("He is pass")
 else:
     print("Fail")
+
+print("Average =", average)
