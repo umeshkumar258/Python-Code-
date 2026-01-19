fb0037cca26aff0a58460fa299f4a34aa4789654
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-01-19 20:05:22 +0530

    Update 04.py

--- 05-Practice-Problems/practice-sets/07/04.py
+++ 05-Practice-Problems/practice-sets/07/04.py
@@ -1,15 +1,3 @@
-# n = int(input("enter the number:"))
-
-# for i in range(2,n):
-#     if(n%i) == 0:
-#         print("it is not prime number")
-#         break
-
-# else:
-#     print("it is prime")
-
-
-
 
 
 
