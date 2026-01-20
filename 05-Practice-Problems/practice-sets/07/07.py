b1d004b1b46c298dc762c520d633002a5dea3d3b
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-01-20 18:15:02 +0530

    Simplify star pyramid pattern implementation
    Refactor star pyramid pattern code for clarity and remove commented-out sections.

--- 05-Practice-Problems/practice-sets/07/07.py
+++ 05-Practice-Problems/practice-sets/07/07.py
@@ -1,32 +1,12 @@
- # it is very important
+# --------------------------------
+# STAR PYRAMID PATTERN
+# --------------------------------
 
+n = int(input("Enter the number: "))
 
-"""  *
-    ***
-   *****
-"""
+for i in range(1, n + 1):
+    # print spaces
+    print(" " * (n - i), end="")
 
-
-# n = int(input("enter the number:"))
-
-# for i in range(1, n+1):
-#     print(" "*(n-i),end ="")
-#     print("*"*(2*i-1),end="")
-#     print("")
-
-
-# n = int(input("enter the number:"))
-
-# for i in range (1,n+1):
-#     print("")
-#     print("*"*(2*i-1),end="")
-#     print(" "* (n-i),end="")
-
-
-n = int(input("Enter the number:"))
-
-
-for i in range(1,n+1):
-    print(""*(n-i),end="")
-    print("*"*(i),end="")
-    print("")
+    # print stars
+    print("*" * (2 * i - 1))
