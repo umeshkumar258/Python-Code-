d42f56e598f5ee27870fae5607c834eb899e3082
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-01-20 18:15:40 +0530

    Refactor star pattern printing with spaces

--- 05-Practice-Problems/practice-sets/07/08.py
+++ 05-Practice-Problems/practice-sets/07/08.py
@@ -1,8 +1,5 @@
-# n = int(input("Enter the number:"))
+n = int(input("Enter the number: "))
 
-
-# for i in range(1,n+1):
-#     print(""*(n-i),end="")
-#     print("*"*(i),end="")
-#     print("")
-
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")   # spaces
+    print("*" * i)                 # stars
