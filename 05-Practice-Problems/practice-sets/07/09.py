2feb494b3a5fb2fef39931d3cb752189d294ed24
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-01-21 20:02:24 +0530

    Fix input prompt and adjust star pattern printing
    Refactor input prompt and improve output formatting.

--- 05-Practice-Problems/practice-sets/07/09.py
+++ 05-Practice-Problems/practice-sets/07/09.py
@@ -1,10 +1,7 @@
-n = int(input("enter the numeber:"))
+n = int(input("Enter the number: "))
 
-for i in range (1,n+1):
-    if(i==1 or i==n):
-        print("*"*n,end="")
+for i in range(1, n + 1):
+    if i == 1 or i == n:
+        print("*" * n)
     else:
-        print("*", end="")
-        print(" "* (n-2),end="")
-        print("*",end="")
-    print("")
+        print("*" + " " * (n - 2) + "*")
