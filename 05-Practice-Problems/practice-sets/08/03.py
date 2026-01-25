9af8bed702e25df52dbf4528b8f3345cf8294d8a
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-01-25 15:32:44 +0530

    Demonstrate various print functions in Python

--- 05-Practice-Problems/practice-sets/08/03.py
+++ 05-Practice-Problems/practice-sets/08/03.py
@@ -1,5 +1,20 @@
+# Normal print (new line after each print)
 print("a")
 print("b")
-print("c",end="")   # cd will prints 
-print("d",end="")
 
+# Using end=""
+print("c", end="")
+print("d")
+
+# Using end with space
+print("e", end=" ")
+print("f")
+
+# Using sep
+print("g", "h", "i", sep="-")
+
+# Combining end and sep
+print("j", "k", sep="*", end=" ")
+print("l")
+
+print("\nThanks")
