89bc81af3cace53c0d1e0a11d86242f862f7d9ea
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-02-14 11:03:05 +0530

    Update 05.py

--- 05-Practice-Problems/practice-sets/10/05.py
+++ 05-Practice-Problems/practice-sets/10/05.py
@@ -1,19 +1,16 @@
-
-
 class Train:
 
-    def __init__(self,trainNo):
+    def __init__(self, trainNo):
         self.trainNo = trainNo
-    
-    def book (self,trainNo,fro,to):
-        print(f"Ticket is booked in train no:{trainNo} 
-        {fro} to {to}")
-    
-    def getstatus(self, trainNo):
-        print(f"Trian no:{trainNo} is running on time")
 
-        
+    def book(self, fro, to):
+        print(f"Ticket is booked in train no: {self.trainNo} from {fro} to {to}")
+
+    def get_status(self):
+        print(f"Train no: {self.trainNo} is running on time")
 
 
 t = Train(525266)
-t.book("kadabagere ","bengaluru")
+
+t.book("Kadabagere", "Bengaluru")
+t.get_status()
