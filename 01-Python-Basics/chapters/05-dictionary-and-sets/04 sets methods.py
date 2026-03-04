916f37b8e775021d956bbb9621c4b4dcf41c08ab
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-03-04 08:36:01 +0530

    Refactor comments and improve print statements

--- 01-Python-Basics/chapters/05-dictionary-and-sets/04 sets methods.py
+++ 01-Python-Basics/chapters/05-dictionary-and-sets/04 sets methods.py
@@ -1,27 +1,63 @@
-# Set properties:
-# 1. Sets are unordered 
-# 2. Sets are unindexed 
-# 3. You cannot change items directly
-# 4. Sets do not allow duplicates
+# -------------------------------
+# Set Properties
+# 1. Sets are unordered
+# 2. Sets are unindexed
+# 3. Items cannot be changed directly
+# 4. Sets do not allow duplicate values
+# -------------------------------
 
+# Creating two sets
 set1 = {37, 393, 283, 29, 66}
 set2 = {35, 72, 283, 9, 66}
 
-set1.add(76)  # Add an element
+print("Set1:", set1)
+print("Set2:", set2)
 
-# Display results
-print("Is set1 a subset of set2?", set1.issubset(set2))
-print("Intersection:", set1.intersection(set2))
+
+# -------------------------------
+# Adding an element
+# -------------------------------
+set1.add(76)
+print("\nAfter adding 76 to set1:", set1)
+
+
+# -------------------------------
+# Set Operations
+# -------------------------------
+
+# Subset check
+print("\nIs set1 a subset of set2?", set1.issubset(set2))
+
+# Superset check
 print("Is set1 a superset of set2?", set1.issuperset(set2))
-print("Union:", set1.union(set2))
 
-# Remove element (do NOT print remove())
+# Intersection (common elements)
+print("Intersection of set1 and set2:", set1.intersection(set2))
+
+# Union (all unique elements)
+print("Union of set1 and set2:", set1.union(set2))
+
+
+# -------------------------------
+# Removing an element
+# -------------------------------
 set1.remove(37)
+print("\nAfter removing 37 from set1:", set1)
 
+
+# -------------------------------
+# Length of the set
+# -------------------------------
 print("Length of set1:", len(set1))
-print("Updated set1:", set1)
 
-# Example: superset check
+
+# -------------------------------
+# Example: Superset check
+# -------------------------------
 s1 = {1, 2, 3}
 s2 = {1, 2, 3, 4, 6}
+
+print("\nExample sets:")
+print("s1:", s1)
+print("s2:", s2)
 print("Is s2 a superset of s1?", s2.issuperset(s1))
