fa83f83b73f7b972afd4646a346c73b11d32c034
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-03-04 08:37:03 +0530

    Improve age validation messages in conditional.py

--- 01-Python-Basics/chapters/06-conditional-expressions/01 conditonal.py
+++ 01-Python-Basics/chapters/06-conditional-expressions/01 conditonal.py
@@ -1,13 +1,15 @@
+# Ask the user to enter age
 age = int(input("Enter your age: "))
 
+# Check age conditions
 if age <= 0:
-    print("You entered an invalid age.")
+    print("Invalid age. Age must be greater than 0.")
 
 elif age < 18:
     print("You are a minor.")
 
 elif age == 18:
-    print("You just reached the age of consent.")
+    print("You have just reached the age of consent.")
 
 else:
     print("You are above the age of consent.")
