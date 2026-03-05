74a79db7a5a740addcba0b52c4b12d5fb8112ceb
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-03-05 18:37:41 +0530

    Refactor age check logic for event entry

--- 01-Python-Basics/chapters/06-conditional-expressions/nested.py
+++ 01-Python-Basics/chapters/06-conditional-expressions/nested.py
@@ -1,9 +1,10 @@
 age = int(input("Enter the age: "))
 
-if age >= 18:
-    if age == 20:
-        print("You can enter the event.")
-    else:
-        print("You need a ticket to enter.")
-else:
+if age < 18:
     print("You are too young to attend the event.")
+
+elif age == 20:
+    print("You can enter the event.")
+
+else:
+    print("You need a ticket to enter.")
