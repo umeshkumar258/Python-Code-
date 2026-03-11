d8444dacba656e8de3ce37ad6fd9df9dbda39109
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-03-11 19:45:31 +0530

    Refactor loops to use variables for range limits

--- 01-Python-Basics/chapters/07-loops/02 while loops.py
+++ 01-Python-Basics/chapters/07-loops/02 while loops.py
@@ -1,7 +1,21 @@
+# -----------------------------------
 # Print numbers from 1 to 50
-for i in range(1, 51):
+# -----------------------------------
+
+start = 1
+end = 50
+
+for i in range(start, end + 1):
     print(i)
 
+
+# -----------------------------------
 # Print even numbers from 2 to 10
-for j in range(2, 11, 2):
+# -----------------------------------
+
+even_start = 2
+even_end = 10
+step = 2
+
+for j in range(even_start, even_end + 1, step):
     print(j)
