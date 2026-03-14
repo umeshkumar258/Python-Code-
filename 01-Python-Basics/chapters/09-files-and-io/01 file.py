87dfe7c73aac53d6cc5762db9cc43aa133d0e314
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-03-14 08:35:35 +0530

    Update 01 file.py

--- 01-Python-Basics/chapters/09-files-and-io/01 file.py
+++ 01-Python-Basics/chapters/09-files-and-io/01 file.py
@@ -1,25 +1,6 @@
 """
-Text files example
-Binary files example
-Email: umeshkumarjn143@gmail.com
+Text file handling example
 """
-
-# ---------------------------------------
-# OPEN & READ A FILE
-# ---------------------------------------
-
-# open() is a built-in function used to open files
-# "file.txt" → file name
-# "r" → read mode (default)
-
-try:
-    with open("file.txt", "r") as f:   # with automatically closes the file
-        data = f.read()
-        print("File content:\n")
-        print(data)
-
-except FileNotFoundError:
-    print("❌ File not found. Please check the file name.")
 
 # ---------------------------------------
 # WRITE TO A FILE
@@ -33,5 +14,16 @@ with open("file.txt", "w") as f:
 with open("file.txt", "a") as f:
     f.write("This line is appended at the end.\n")
 
-print("\nFile operations completed successfully.")
+# ---------------------------------------
+# OPEN & READ A FILE
+# ---------------------------------------
+try:
+    with open("file.txt", "r") as f:
+        data = f.read()
+        print("File content:\n")
+        print(data)
 
+except FileNotFoundError:
+    print("File not found. Please check the file name.")
+
+print("\nFile operations completed successfully.")
