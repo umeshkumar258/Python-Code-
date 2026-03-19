9d5bf274aff2c9cbaccdb3db848e44dd6733994b
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-03-19 11:03:35 +0530

    Modify input prompt and add confirmation message
    Updated input prompt and added a print statement.

--- 03-Advanced-Python/advanced/practice-set-01/05.py
+++ 03-Advanced-Python/advanced/practice-set-01/05.py
@@ -1,18 +1,14 @@
-# ---------------------------------------
-# MULTIPLICATION TABLE TO FILE
-# ---------------------------------------
 
-n = int(input("Enter the number: "))
+
+n = int(input("Enter the number the values is there: "))
 
 # Generate table using list comprehension
 table = [n * i for i in range(1, 11)]
 
-# ---------------------------------------
-# APPEND TABLE TO FILE (formatted)
-# ---------------------------------------
 with open("table.txt", "a") as f:
     f.write(f"\nMultiplication Table of {n}\n")
     for i, value in enumerate(table, start=1):
         f.write(f"{n} x {i} = {value}\n")
 
 print("✅ Table written to file successfully.")
+print("it was good")
