46a632c1b284d0ade083a2aebeef53ee96baa129
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-04-09 21:44:02 +0530

    Refactor divide_numbers function for error handling

--- 03-Advanced-Python/advanced/practice-set-01/04.py
+++ 03-Advanced-Python/advanced/practice-set-01/04.py
@@ -1,18 +1,19 @@
+def divide_numbers():
+    try:
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+
+        result = a / b
+
+    except ValueError:
+        print("❌ Please enter valid integers only.")
+    except ZeroDivisionError:
+        print("❌ Cannot divide by zero (infinite).")
+    else:
+        print(f"✅ Division successful. Result = {result:.2f}")
+    finally:
+        print("🔚 Program finished.")
 
 
-try:
-    a = int(input("Enter the first number: "))
-    b = int(input("Enter the second number: "))
-
-    result = a / b
-    print(f"The division is: {result}")
-
-except ZeroDivisionError:
-    print("❌ Cannot divide by zero (infinite).")
-
-except ValueError:
-    print("❌ Please enter valid integers only.")
-
-else:
-    print("✅ Division successful.")
-
+# Run the function
+divide_numbers()
