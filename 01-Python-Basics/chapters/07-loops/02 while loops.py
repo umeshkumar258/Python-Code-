724e1639a0eddcc0d3b6e3b61d30e5585b9a9b09
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-04-11 22:48:42 +0530

    Refactor number printing to use print_number_range function
    Refactored code to use a function for printing number ranges and added descriptive labels.

--- 01-Python-Basics/chapters/07-loops/02 while loops.py
+++ 01-Python-Basics/chapters/07-loops/02 while loops.py
@@ -1,21 +1,16 @@
-# -----------------------------------
-# Print numbers from 1 to 50
-# -----------------------------------
+def print_number_range(start, end, step=1, label="Numbers"):
+    """
+    Prints a range of numbers with a descriptive label.
+    """
+    print(f"--- {label} ({start} to {end}) ---")
+    
+    # We use end + 1 because range is exclusive of the stop parameter
+    for i in range(start, end + 1, step):
+        print(i)
 
-start = 1
-end = 50
+# 1. Print numbers 1 to 50
+print_number_range(1, 50)
 
-for i in range(start, end + 1):
-    print(i)
-
-
-# -----------------------------------
-# Print even numbers from 2 to 10
-# -----------------------------------
-
-even_start = 2
-even_end = 10
-step = 2
-
-for j in range(even_start, even_end + 1, step):
-    print(j)
+# 2. Print even numbers 2 to 10
+# Using the default 'step' for the first call and overriding it here
+print_number_range(2, 10, step=2, label="Even Numbers")
