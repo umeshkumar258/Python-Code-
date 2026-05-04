1c009ee380de9e976a5965c1b4a683d40150a9d9
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-04 10:03:22 +0530

    Fix indentation in multiplication table loop

--- 05-Practice-Problems/practice-sets/07/03.py
+++ 05-Practice-Problems/practice-sets/07/03.py
@@ -1,18 +1,6 @@
-# --------------------------------
-# MULTIPLICATION TABLE USING WHILE
-# --------------------------------
-
-# Taking input from user
 n = int(input("Enter the number: "))
-
-# Initial value
 i = 1
 
-# Loop condition
 while i <= 10:
-    print(f"{n} × {i} = {n * i}")
+    print(f"{n} × {i} = {n*i}")
     i += 1
-
-# --------------------------------
-# End of program
-# --------------------------------
