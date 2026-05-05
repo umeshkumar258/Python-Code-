6c06f86335b4c8763c2ad7207f162109c723a274
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-05 16:22:45 +0530

    Refactor star pyramid pattern printing logic

--- 05-Practice-Problems/practice-sets/07/07.py
+++ 05-Practice-Problems/practice-sets/07/07.py
@@ -1,12 +1,6 @@
-# --------------------------------
-# STAR PYRAMID PATTERN
-# --------------------------------
-
 n = int(input("Enter the number: "))
 
 for i in range(1, n + 1):
-    # print spaces
-    print(" " * (n - i), end="")
-
-    # print stars
-    print("*" * (2 * i - 1))
+    spaces = n - i
+    stars = 2 * i - 1
+    print(" " * spaces + "*" * stars)
