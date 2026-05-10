a51aa656c056b7547ea9740a6bff7aa02fcc0d28
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-10 09:51:30 +0530

    Implement reverse multiplication table
    Replaced star pattern printing with a reverse multiplication table.

--- 05-Practice-Problems/practice-sets/07/11 .py
+++ 05-Practice-Problems/practice-sets/07/11 .py
@@ -1,42 +1,7 @@
-# ---------------------------------------
-# ALL IN ONE STAR PATTERNS
-# ---------------------------------------
+# Reverse Multiplication Table
 
 n = int(input("Enter the number: "))
 
-print("\n1) Right Triangle")
-for i in range(1, n + 1):
-    print("*" * i)
-
-
-print("\n2) Inverted Right Triangle")
-for i in range(1, n + 1):
-    print("*" * (n - i + 1))
-
-
-print("\n3) Left Aligned Triangle")
-for i in range(1, n + 1):
-    print(" " * (n - i) + "*" * i)
-
-
-print("\n4) Center Pyramid")
-for i in range(1, n + 1):
-    print(" " * (n - i) + "*" * (2 * i - 1))
-
-
-print("\n5) Inverted Pyramid")
-for i in range(n, 0, -1):
-    print(" " * (n - i) + "*" * (2 * i - 1))
-
-
-print("\n6) Hollow Square")
-for i in range(1, n + 1):
-    if i == 1 or i == n:
-        print("*" * n)
-    else:
-        print("*" + " " * (n - 2) + "*")
-
-
-print("\n7) Square Pattern")
-for i in range(n):
-    print("*" * n)
+for i in range(10, 0, -1):
+    result = n * i
+    print(f"{n} × {i} = {result}")
