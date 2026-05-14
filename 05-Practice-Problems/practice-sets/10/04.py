6f97eceaaf50ab5834a89e6dd4bc496746830f32
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-14 14:01:28 +0530

    Refactor Calculator class for improved readability

--- 05-Practice-Problems/practice-sets/10/04.py
+++ 05-Practice-Problems/practice-sets/10/04.py
@@ -1,23 +1,28 @@
 class Calculator:
-    def __init__(self, n):
-        self.n = n
+    def __init__(self, number):
+        self.number = number
 
     def square(self):
-        print(f"The square is {self.n ** 2}")
+        print("Square:", self.number ** 2)
 
     def cube(self):
-        print(f"The cube is {self.n ** 3}")
-    
+        print("Cube:", self.number ** 3)
+
     def square_root(self):
-        print(f"The square root is {self.n ** 0.5}")
+        print("Square Root:", self.number ** 0.5)
 
     @staticmethod
     def hello():
         print("Hello Umesh")
 
 
-a = Calculator(4)
-a.square()
-a.cube()
-a.square_root()
-a.hello()
+# Object
+calc = Calculator(4)
+
+# Method calls
+calc.square()
+calc.cube()
+calc.square_root()
+
+# Static method call
+calc.hello()
