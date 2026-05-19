56570501ed458e1b40f82690301fc770b3f2610e
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-19 11:10:19 +0530

    Refactor multilevel inheritance example
    Refactor print statements for better readability and remove unused object instantiations.

--- 01-Python-Basics/chapters/11-inheritance/03 multilevel inheritance.py
+++ 01-Python-Basics/chapters/11-inheritance/03 multilevel inheritance.py
@@ -1,61 +1,39 @@
-# ---------------------------------------
-# BASE CLASS
-# ---------------------------------------
+# Parent Class
 class Employee:
+
     def __init__(self):
         self.a = 1
 
     def show(self):
-        print(f"a = {self.a}")
+        print("a =", self.a)
 
 
-# ---------------------------------------
-# CHILD CLASS (inherits Employee)
-# ---------------------------------------
+# Child Class
 class Programmer(Employee):
+
     def __init__(self):
-        super().__init__()   # Call parent constructor
+        super().__init__()
         self.b = 2
 
     def show(self):
         super().show()
-        print(f"b = {self.b}")
+        print("b =", self.b)
 
 
-# ---------------------------------------
-# GRANDCHILD CLASS (inherits Programmer)
-# ---------------------------------------
+# Grandchild Class
 class Manager(Programmer):
+
     def __init__(self):
         super().__init__()
         self.c = 3
 
     def show(self):
         super().show()
-        print(f"c = {self.c}")
+        print("c =", self.c)
 
 
-# ---------------------------------------
-# OBJECT OF Employee
-# ---------------------------------------
-print("Employee object:")
-o1 = Employee()
-o1.show()
-print()
+# Object
+m = Manager()
 
-
-# ---------------------------------------
-# OBJECT OF Programmer
-# ---------------------------------------
-print("Programmer object:")
-o2 = Programmer()
-o2.show()
-print()
-
-
-# ---------------------------------------
-# OBJECT OF Manager
-# ---------------------------------------
-print("Manager object:")
-o3 = Manager()
-o3.show()
+# Output
+m.show()
