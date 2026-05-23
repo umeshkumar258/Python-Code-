80ad06ec5e04927948e4dc7cf5194a0c21f0a324
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-23 11:11:53 +0530

    Refactor multiplication function into a new definition

--- 05-Practice-Problems/practice-sets/08/08.py
+++ 05-Practice-Problems/practice-sets/08/08.py
@@ -1,7 +1,7 @@
-n = int(input("Enter the number: "))
+def multiplication_table(n):
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
 
-def mul(n):
-    for i in range(1,11):
-        print(f"{n}X{i} ={n*i}")
+num = int(input("Enter a number: "))
 
-mul(n)
+multiplication_table(num)
