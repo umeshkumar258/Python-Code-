a6a55714e37345389e68f071aaefb2d2fde412cb
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-28 08:49:41 +0530

    Update 02 file write.py

--- 01-Python-Basics/chapters/09-files-and-io/02 file write.py
+++ 01-Python-Basics/chapters/09-files-and-io/02 file write.py
@@ -1,8 +1,17 @@
-content = "I am Umesh Kumar J B. I study at REVA University. I am in CSIT. Yes, I am good. Back to work."
+# Content to write into the file
+content = """
+I am Umesh Kumar J B.
+I study at REVA University.
+I am in CSIT.
+Yes, I am good.
+Back to work.
+"""
 
+# File name
 filename = "newfile.txt"
 
+# Write content into the file
 with open(filename, "w") as file:
     file.write(content)
 
-print("Data written to the file successfully.")
+print("✅ Data written to the file successfully.")
