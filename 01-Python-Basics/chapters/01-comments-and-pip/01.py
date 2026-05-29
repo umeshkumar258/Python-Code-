a5c225dfecc0f900c54c509648998b897b606d64
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-29 08:58:26 +0530

    Update 01.py

--- 01-Python-Basics/chapters/01-comments-and-pip/01.py
+++ 01-Python-Basics/chapters/01-comments-and-pip/01.py
@@ -1,33 +1,14 @@
-"""
-Author: Umesh Kumar J B
-Description: Basic Python program demonstrating
-- Printing messages
-- Multi-line comments
-- Taking user input
-- Type conversion
-"""
+# Author: Umesh Kumar J B
 
-def main():
-    # Welcome Messages
-    print("Hello Umesh!")
-    print("Hello Umesh, welcome to Python learning!\n")
+print("Hello Umesh!")
+print("Welcome to Python Learning!\n")
 
-    # Personal Introduction
-    intro = """
-    I am Umesh Kumar J B.
-    I am pursuing Engineering at REVA University.
-    """
-    print(intro)
+print("I am Umesh Kumar J B.")
+print("I am pursuing Engineering at REVA University.\n")
 
-    # Taking user input safely
-    try:
-        number = int(input("Enter a number: "))
-        print(f"You entered: {number} | Type: {type(number)}")
-    except ValueError:
-        print("Invalid input! Please enter a valid integer.")
+number = int(input("Enter a number: "))
 
-    print("\nUmesh is continuously learning and improving!")
+print("You entered:", number)
+print("Type:", type(number))
 
-# Standard Python practice
-if __name__ == "__main__":
-    main()
+print("\nUmesh is continuously learning and improving!")
