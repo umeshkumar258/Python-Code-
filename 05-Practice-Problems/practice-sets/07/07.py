19654530c58f2c8b22dd72a0af7add4b9178f1d9
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-31 09:17:32 +0530

    Refactor pyramid printing logic in 07.py

--- 05-Practice-Problems/practice-sets/07/07.py
+++ 05-Practice-Problems/practice-sets/07/07.py
@@ -1,6 +1,4 @@
-n = int(input("Enter the number: "))
+n = int(input("Enter the number of rows: "))
 
-for i in range(1, n + 1):
-    spaces = n - i
-    stars = 2 * i - 1
-    print(" " * spaces + "*" * stars)
+for row in range(1, n + 1):
+    print(" " * (n - row) + "*" * (2 * row - 1))
