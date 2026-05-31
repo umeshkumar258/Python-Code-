5019c749d686743fb304cdbd98bcf845719dae6e
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-31 09:17:02 +0530

    Fix factorial calculation and input prompt

--- 05-Practice-Problems/practice-sets/07/06.py
+++ 05-Practice-Problems/practice-sets/07/06.py
@@ -1,9 +1,11 @@
-n = int(input("Enter the number: "))
+n = int(input("Enter a number: "))
 
 if n < 0:
     print("Factorial is not defined for negative numbers")
 else:
     fact = 1
-    for i in range(2, n + 1):
+
+    for i in range(1, n + 1):
         fact *= i
-    print(f"Factorial of {n} is {fact}")
+
+    print("Factorial =", fact)
