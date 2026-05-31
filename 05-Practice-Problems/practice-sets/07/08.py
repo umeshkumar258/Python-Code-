32b759e720b7ec6e97ade864a33dcd9d4d7deba7
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-05-31 09:18:06 +0530

    Update 08.py

--- 05-Practice-Problems/practice-sets/07/08.py
+++ 05-Practice-Problems/practice-sets/07/08.py
@@ -1,6 +1,4 @@
-n = int(input("Enter the number: "))
+n = int(input("Enter the number of rows: "))
 
 for i in range(1, n + 1):
-    spaces = n - i
-    stars = i
-    print(" " * spaces + "*" * stars)
+    print(" " * (n - i) + "*" * i)
