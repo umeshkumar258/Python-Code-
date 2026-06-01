4c4db8b00b290fc87a87db58a3edd33bbc41e0b9
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-06-01 11:49:30 +0530

    Refactor age check logic and error handling

--- 01-Python-Basics/chapters/06-conditional-expressions/02 multiple if statements.py
+++ 01-Python-Basics/chapters/06-conditional-expressions/02 multiple if statements.py
@@ -1,25 +1,16 @@
-def check_age():
-    try:
-        age = int(input("Enter your age: ").strip())
+try:
+    age = int(input("Enter your age: "))
 
-        if age <= 0:
-            print("❗ Invalid age entered. Age must be greater than 0.")
+    if age <= 0:
+        print("Invalid age")
+    elif age < 18:
+        print("You are not old enough")
+    elif age <= 98:
+        print("You are doing good")
+    elif age == 99:
+        print("Wow! You are 99 years old!")
+    else:
+        print("You are above 99")
 
-        elif age < 18:
-            print("⚠️ You are not old enough.")
-
-        elif age <= 98:
-            print("🙂 You are doing good.")
-
-        elif age == 99:
-            print("🎉 Wow! You are 99 years old! Ok buddy.")
-
-        else:
-            print("👋 Welcome! You are above 99.")
-
-    except ValueError:
-        print("❗ Please enter a valid number for age.")
-
-# Run the function
-if __name__ == "__main__":
-    check_age()
+except ValueError:
+    print("Please enter a valid age")
