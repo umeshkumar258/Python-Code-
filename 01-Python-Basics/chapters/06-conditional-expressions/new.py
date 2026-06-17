21a620c015fa2c5ab368af792a7cfaff6a4a9439
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-06-17 10:07:09 +0530

    Simplify print statements in age check program

--- 01-Python-Basics/chapters/06-conditional-expressions/new.py
+++ 01-Python-Basics/chapters/06-conditional-expressions/new.py
@@ -1,17 +1,19 @@
+# Simple Age Check Program
+
 try:
     age = int(input("Enter your age: "))
 
     if age < 18:
-        print("You are too young to attend the event.")
+        print("Too young to attend.")
 
     elif age == 20:
-        print("You can enter the event.")
+        print("You can enter.")
 
     elif age > 20:
-        print("You need a ticket to enter.")
+        print("You need a ticket.")
 
     else:
-        print("You can attend, but rules may apply.")
+        print("Rules may apply.")
 
 except ValueError:
-    print("❗ Please enter a valid number for age.")
+    print("Please enter a valid age.")
